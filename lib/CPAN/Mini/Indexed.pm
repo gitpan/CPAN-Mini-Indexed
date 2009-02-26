5034fefae1e401bfe124f888069fd17acb2843bf
نديم ابن ﺤﻣﻮﺪﺓ الخمير - Nadim Khemir <nadim.khemir@gmail.com>
2009-02-26 23:27:52 +0000

    Import of NKH/CPAN-Mini-Indexed-0.03_01.2 from CPAN.
    gitpan-cpan-distribution: CPAN-Mini-Indexed
    gitpan-cpan-version:      0.03_01.2
    gitpan-cpan-path:         NKH/CPAN-Mini-Indexed-0.03_01.2.tar.gz
    gitpan-cpan-author:       NKH
    gitpan-cpan-maturity:     developer

--- lib/CPAN/Mini/Indexed.pm
+++ lib/CPAN/Mini/Indexed.pm
@@ -9,7 +9,7 @@ BEGIN
 {
 use Sub::Exporter -setup => 
 	{
-	exports => [ qw() ],
+	exports => [ qw(search check_index show_database_information) ],
 	groups  => 
 		{
 		all  => [ qw() ],
@@ -17,10 +17,23 @@ use Sub::Exporter -setup =>
 	};
 	
 use vars qw ($VERSION);
-$VERSION     = '0.02_01';
+$VERSION     = '0.03_01';
 }
 
 #-------------------------------------------------------------------------------
+
+use Time::HiRes 'time' ;
+use File::Temp ;
+use Text::Pluralize;
+use File::Find::Rule ;
+use IO::Zlib ;
+use Archive::Tar ;
+use File::Copy ;
+
+use Search::Indexer::Incremental::MD5 qw() ;
+use Search::Indexer::Incremental::MD5::Indexer qw() ;
+use Search::Indexer::Incremental::MD5::Searcher qw() ;
+use Search::Indexer::Incremental::MD5::Language::Perl qw() ;
 
 use English qw( -no_match_vars ) ;
 
@@ -46,191 +59,507 @@ This module implements ...
 
 =cut
 
+#----------------------------------------------------------------------------------------------------------
 
-#-------------------------------------------------------------------------------
-
-Readonly my $NEW_ARGUMENTS => [qw(NAME INTERACTION)] ;
-
-sub new
+sub show_database_information
 {
 
-=head2 new( xxx )
+=head2 ( )
 
-Create a CPAN::Mini::Indexed .  
-
-  my $object = new CPAN::Mini::Indexed() ;
+  some code
 
 I<Arguments>
 
 =over 2 
 
-=item * $xxx - 
+=item * $ - 
 
 =back
 
 I<Returns> - Nothing
 
-I<Exceptions>
-
-See C<xxx>.
+I<Exceptions> - None
 
 =cut
 
-my ($invocant, @setup_data) = @_ ;
+my ($options) = @_ ;
+my $information = Search::Indexer::Incremental::MD5::show_database_information($options->{index_directory}) ;
 
-my $class = ref($invocant) || $invocant ;
-confess 'Invalid constructor call!' unless defined $class ;
+# make sizes more readable
+1 while $information->{entries} =~ s/^([-+]?\d+)(\d{3})/$1_$2/ ;
+1 while $information->{size} =~ s/^([-+]?\d+)(\d{3})/$1_$2/ ;
 
-my $object = {} ;
-
-my ($package, $file_name, $line) = caller() ;
-bless $object, $class ;
-
-$object->Setup($package, $file_name, $line, @setup_data) ;
-
-return($object) ;
+print {*STDOUT} <<"EOI" ;
+Location: $options->{index_directory}
+Last updated on: $information->{update_date}
+Number of indexed documents: $information->{entries}
+Database size: $information->{size} bytes
+EOI
 }
 
-#-------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------
 
-sub Setup
+sub search
 {
 
-=head2 Setup
+=head2 ( )
 
-Helper sub called by new. This is a private sub.
+  some code
+
+I<Arguments>
+
+=over 2 
+
+=item * $ - 
+
+=back
+
+I<Returns> - Nothing
+
+I<Exceptions> - None
 
 =cut
 
-my ($self, $package, $file_name, $line, @setup_data) = @_ ;
+my ($options) = @_ ;
 
-if (@setup_data % 2)
-	{
-	croak "Invalid number of argument '$file_name, $line'!" ;
-	}
-
-$self->CheckOptionNames($NEW_ARGUMENTS, @setup_data) ;
-
-%{$self} = 
-	(
-	NAME                   => 'Anonymous',
-	FILE                   => $file_name,
-	LINE                   => $line,
-	
-	@setup_data,
-	) ;
-
-my $location = "$self->{FILE}:$self->{LINE}" ;
-
-$self->{INTERACTION}{INFO} ||= sub {print @_} ;
-$self->{INTERACTION}{WARN} ||= \&Carp::carp ;
-$self->{INTERACTION}{DIE}  ||= \&Carp::confess ;
-
-
-if($self->{VERBOSE})
-	{
-	$self->{INTERACTION}{INFO}('Creating ' . ref($self) . " '$self->{NAME}' at $location.\n") ;
-	}
-
-return(1) ;
-}
-
-#-------------------------------------------------------------------------------
-
-sub CheckOptionNames
-{
-
-=head2 CheckOptionNames
-
-Verifies the named options passed to the members of this class. Calls B<{INTERACTION}{DIE}> in case
-of error. This shall not be used directly.
-
-=cut
-
-my ($self, $valid_options, @options) = @_ ;
-
-if (@options % 2)
-	{
-	$self->{INTERACTION}{DIE}->('Invalid number of argument!') ;
-	}
-
-if('HASH' eq ref $valid_options)
-	{
-	# OK
-	}
-elsif('ARRAY' eq ref $valid_options)
-	{
-	$valid_options = map{$_ => 1} @{$valid_options} ;
-	}
-else
-	{
-	$self->{INTERACTION}{DIE}->("Invalid argument '$valid_options'!") ;
-	}
-
-my %options = @options ;
-
-for my $option_name (keys %options)
-	{
-	unless(exists $valid_options->{$option_name})
+my $searcher 
+	= eval 
 		{
-		$self->{INTERACTION}{DIE}->("$self->{NAME}: Invalid Option '$option_name' at '$self->{FILE}:$self->{LINE}'!")  ;
+		Search::Indexer::Incremental::MD5::Searcher->new
+			(
+			INDEX_DIRECTORY => $options->{index_directory}, 
+			USE_POSITIONS => 0, 
+			WORD_REGEX => qr/\w+/,
+			);
+		} or croak "No full text index found! $EVAL_ERROR\n" ;
+
+my $results  = $searcher->search(SEARCH_STRING => $options->{search}) ;
+
+my @indexes = map { $_->[0] }
+				reverse
+					sort { $a->[1] <=> $b->[1] }
+						map { [$_, $results->[$_]{SCORE}] }
+							0 .. $#$results ;
+
+my ($displayed_matches, %displayed_module) = (0) ;
+
+for my $index (@indexes)
+	{
+	last if $displayed_matches++ == $options->{lines} ;
+	
+	my $matching_file = $results->[$index]{PATH} ;
+	
+	unless($matching_file)
+		{
+		carp "matched id:'$results->[$index]{ID}' which was removed!\n" ;
+		next ;
+		}
+	
+	(my $matching_file_short = $matching_file) =~ s{^/tmp/[^/]+/}{} ;
+	
+	if($options->{modules_only})
+		{
+		(my $matching_module = $matching_file_short) =~ s{^([^/]+).*}{$1} ;
+		$matching_module =~ s/(.*)-.*/$1/g ;
+		$matching_module =~ s/-/::/g ;
+		
+		print {*STDOUT} "$matching_module\n" unless exists $displayed_module{$matching_module} ;
+		
+		$displayed_module{$matching_module} += $results->[$index]{SCORE} ;
+		}
+	else
+		{
+		if($options->{verbose})
+			{
+			print {*STDOUT} "'$matching_file_short' [id:'$results->[$index]{ID}', score: '$results->[$index]{SCORE}]'\n" ;
+			}
+		else
+			{
+			print {*STDOUT} "$matching_file_short\n" ;
+			}
+		}
+	}
+}
+
+#----------------------------------------------------------------------------------------------------------
+
+sub check_index
+{
+
+=head2 check_index($indexer, $options)
+
+ brings the cpan mini index database up to date
+
+I<Arguments> - 
+
+$indexer, $options
+
+I<Returns> -  Nothing
+
+I<Exceptions> - 
+
+=cut
+
+my ($options) = @_ ;
+
+my $cpan_mini = $options->{cpan_mini} || $ENV{CPAN_MINI} || '/devel/cpan' ;
+$cpan_mini =~ s{^\./}[] ;
+$cpan_mini =~ s{/$}[] ;
+
+croak "Invalid cpan mini repository!\n" if $cpan_mini eq $EMPTY_STRING;
+
+printf "[CPAN mini repository in '$cpan_mini']\n" if ($options->{verbose}) ;
+
+my $modules_details_file = "$cpan_mini/modules/02packages.details.txt.gz" ;
+my $cache_details_file = "$options->{index_directory}/02packages.details.txt.gz" ;
+
+if(index_needs_update($modules_details_file, $cache_details_file))
+	{
+	my @stopwords = (STOPWORDS => $options->{stopwords_file}) if $options->{stopwords_file} ;
+
+	my $indexer = Search::Indexer::Incremental::MD5::Indexer->new
+					(
+					INDEX_DIRECTORY => $options->{index_directory}, 
+					USE_POSITIONS => 0, 
+					Search::Indexer::Incremental::MD5::Language::Perl::get_perl_word_regex_and_stopwords(),
+					@stopwords,
+					) ;
+
+	my ($modules_in_repository, $modules_up_to_date, $modules_out_of_date) = scan_index($cpan_mini, $indexer) ;
+
+	remove_out_of_date_modules($indexer, $modules_out_of_date, $options) ;
+
+	my %new_modules = grep { ! exists $modules_up_to_date->{$_} } keys %{$modules_in_repository};
+
+	add_new_modules($indexer, $cpan_mini, \%new_modules, $options) ;
+
+	if(-e $modules_details_file)
+		{
+		copy($modules_details_file, $cache_details_file) or carp "Warning: '$cache_details_file' creation failed: $!" ;
 		}
 	}
 
-if
-	(
-	   (defined $options{FILE} && ! defined $options{LINE})
-	|| (!defined $options{FILE} && defined $options{LINE})
-	)
-	{
-	$self->{INTERACTION}{DIE}->("$self->{NAME}: Incomplete option FILE::LINE!") ;
-	}
-
-return(1) ;
+return ;
 }
 
-#-------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------
 
-sub A
+sub index_needs_update
 {
 
-=head2 A( xxx )
+=head2 ( )
 
-xxx description
-
-  xxx some code
+  some code
 
 I<Arguments>
 
 =over 2 
 
-=item * $xxx - 
+=item * $ - 
 
 =back
 
 I<Returns> - Nothing
 
-I<Exceptions>
-
-See C<xxx>.
+I<Exceptions> - None
 
 =cut
 
-my ($self) = @_ ;
+my ($modules_details_file, $cache_details_file) = @_ ;
 
-if(defined $self && __PACKAGE__ eq ref $self)
+my $index_need_update = 1 ;
+
+if(-e $modules_details_file && -e $cache_details_file)
 	{
-	# object sub
-	my ($var, $var2) = @_ ;
+	if
+		(
+		Search::Indexer::Incremental::MD5::get_file_MD5($modules_details_file) 
+			eq Search::Indexer::Incremental::MD5::get_file_MD5($cache_details_file)
+		)
+		{
+		$index_need_update = 0 ; 
+		}
+	}
 	
-	}
-else	
+return $index_need_update ;
+}
+
+#----------------------------------------------------------------------------------------------------------
+
+sub scan_index
+{
+
+=head2 ( )
+
+  some code
+
+I<Arguments>
+
+=over 2 
+
+=item * $ - 
+
+=back
+
+I<Returns> - Nothing
+
+I<Exceptions> - None
+
+=cut
+
+my ($cpan_mini, $indexer) = @_ ;
+
+my %modules_in_repository
+	= map {chomp($_) ; $_ => 1} 
+		File::Find::Rule
+			->file()
+			->name('*.tar.gz')
+			->in($cpan_mini);
+
+
+my (%modules_up_to_date, %indexed_modules_to_remove) ;
+
+$indexer->check_indexed_files
+		(
+		DONE_ONE_FILE_CALLBACK =>
+			sub 
+			{
+			my ($file, $description, $file_info) = @_ ;			
+			
+			if(exists $modules_in_repository{"$cpan_mini/$description"})
+				{
+				# we can't delete $modules_in_repository{$cpan_mini . $description} as
+				# it may contain multiple indexed files
+				$modules_up_to_date{"$cpan_mini/$description"}++ ;
+				}
+			else
+				{
+				$indexed_modules_to_remove{$description}{$file} = $file_info->{ID} ;
+				}
+			},
+		) ;
+		
+return (\%modules_in_repository, \%modules_up_to_date, \%indexed_modules_to_remove) ;
+}
+
+#----------------------------------------------------------------------------------------------------------
+
+sub remove_out_of_date_modules
+{
+
+=head2 ( )
+
+  some code
+
+I<Arguments>
+
+=over 2 
+
+=item * $ - 
+
+=back
+
+I<Returns> - Nothing
+
+I<Exceptions> - None
+
+=cut
+
+my ($indexer, $modules_out_of_date, $options) = @_ ;
+
+my $t0_remove = time ;
+
+my $number_of_modules = scalar(keys %{$modules_out_of_date}) ;
+my $module_index = 0 ;
+my $total_number_of_files = 0 ;
+
+for my $module_to_remove(sort keys %{$modules_out_of_date})
 	{
-	# class sub
-	unshift @_, $self ;
+	my $t0_remove_module = time ;
+	
+	$module_index++ ;
+	print "-$module_to_remove\n" ;
+	
+	my $number_of_files_in_module = 0 ;
+	
+	for my $module_element (sort keys %{$modules_out_of_date->{$module_to_remove}})
+		{
+		(my $module_element_short = $module_element) =~ s{^/tmp/[^/]+/}[] ;
+		
+		$total_number_of_files++ ;
+		$number_of_files_in_module++ ;
+		
+		print "\t-$module_element_short\n" if $options->{verbose} ;
+		
+		$indexer->remove_document_with_id($modules_out_of_date->{$module_to_remove}{$module_element})   ;
+		}
+		
+	if ($options->{verbose})
+		{
+		printf
+			"\t[$module_index/$number_of_modules ($number_of_files_in_module) in %.3f s.]\n",
+			(time - $t0_remove_module)  ;
+		}
 	}
 
-return(0) ;
+if ($options->{verbose})
+	{
+	printf "[Removed $total_number_of_files files in $number_of_modules modules in %.3f s.]\n", (time - $t0_remove) ; 
+	}
+}
+
+#----------------------------------------------------------------------------------------------------------
+
+sub add_new_modules
+{
+
+=head2 ( )
+
+  some code
+
+I<Arguments>
+
+=over 2 
+
+=item * $ - 
+
+=back
+
+I<Returns> - Nothing
+
+I<Exceptions> - None
+
+=cut
+
+my ($indexer, $cpan_mini, $new_modules, $options) = @_ ;
+
+my $module_index = 0 ;
+my $total_number_of_files = 0 ;
+my $number_of_modules = scalar(keys %{$new_modules}) ;
+
+my $one_warning = 0 ;
+local $SIG{__WARN__} = get_sig_warn_sub(\$one_warning) ;
+
+my $t0_add_modules = time;
+
+for my $module (sort keys %{$new_modules})
+	{
+	my $t0_module = time ;
+	
+	$one_warning = 0 ;
+	$module_index++ ;
+	
+	(my $module_to_add_short = $module) =~ s{^$cpan_mini/}[] ;
+	print "+$module_to_add_short\n" ;
+
+	my $directory = File::Temp->newdir() ;
+	my $extraction_directory = $directory->dirname;
+
+	my $next_archive_item = Archive::Tar->iter($module, 1);
+
+	while(my $item = $next_archive_item->()) 
+		{
+		my $item_name = $item->name() ;
+		$item->extract("$extraction_directory/$item_name") 
+			or carp "Error: failed Extracting '$item_name' from '$module'!\n";
+		}	
+	
+	my @files = File::Find::Rule->file()->name( '*.pod', '*.pl', '*.pm')->in($extraction_directory);
+
+	my $number_of_files_in_module = scalar(@files) ;
+	$total_number_of_files += $number_of_files_in_module ;
+	
+	my $t0_index = time ;
+	
+	for my $file (@files)
+		{
+		(my $file_short = $file) =~ s{^/tmp/[^/]+/}{} ;
+		print "\t+$file_short\n" if ($options->{verbose}) ;
+			
+		$indexer->add_files
+			(
+			FILES => [map { {NAME => $_, DESCRIPTION => $module_to_add_short} } $file],
+			MAXIMUM_DOCUMENT_SIZE => $options->{maximum_document_size},
+			) ;
+		}
+		
+	if ($options->{verbose})
+		{
+		printf
+			"\t[$module_index/$number_of_modules ($number_of_files_in_module) in "
+			. "%.3f s. (indexing: %.3f s.)]\n",
+			(time - $t0_module), (time - $t0_index)  ;
+		}
+	}
+	
+if ($options->{verbose})
+	{
+	print {*STDOUT} 
+		pluralize("[Re-indexed $total_number_of_files file(s) in ", $total_number_of_files),
+		pluralize("$number_of_modules module(s) in ", $number_of_modules),
+		sprintf("%.3f s.]\n", (time - $t0_add_modules)) ;
+	}
+}
+
+#----------------------------------------------------------------------------------------------------------
+
+sub get_sig_warn_sub
+{
+
+=head2 ( )
+
+  some code
+
+I<Arguments>
+
+=over 2 
+
+=item * $ - 
+
+=back
+
+I<Returns> - Nothing
+
+I<Exceptions> - None
+
+=cut
+
+my ($one_warning) = @_ ;
+
+return 
+	sub
+		{
+		my ($warning) = @_ ;
+		
+		if
+			(
+			$warning =~ /^Invalid header block at offset unknown/
+			|| $warning =~ /^Couldn't read chunk/
+			|| $warning =~ /checksum error/
+			)
+			{
+			if(! $$one_warning)
+				{
+				print "\tInvalid Archive!\n" ;
+				$$one_warning++ ;
+				}
+			else
+				{
+				# ignore
+				}
+			}
+		else
+			{
+			if($warning =~ m~'/tmp/.+?/(.+)' is bigger than .+ bytes, skipping!~)
+				{
+				print "\tSkipping '$1', too big!\n" ;
+				}
+			else
+				{
+				warn $warning ;
+				}
+			}
+		} ;
 }
 
 #-------------------------------------------------------------------------------
--- t/001_load.t
+++ t/001_load.t
@@ -10,16 +10,3 @@ use Test::Exception ;
 
 BEGIN { use_ok( 'CPAN::Mini::Indexed' ) or BAIL_OUT("Can't load module"); } ;
 
-my $object = new CPAN::Mini::Indexed ;
-
-is(defined $object, 1, 'default constructor') ;
-isa_ok($object, 'CPAN::Mini::Indexed');
-
-my $new_config = $object->new() ;
-is(defined $new_config, 1, 'constructed from object') ;
-isa_ok($new_config , 'CPAN::Mini::Indexed');
-
-dies_ok
-	{
-	CPAN::Mini::Indexed::new () ;
-	} "invalid constructor" ;
